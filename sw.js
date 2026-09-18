e1ec39ffd57d25e85538c7a62d9035612819227b
Sarapio <buganjaines@gmail.com>
2026-09-18 18:47:30 +0300

    Refactor service worker for caching strategy

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(()=>{})); });
+const CACHE="one-sarapio-v6";const FILES=["./","./index.html","./manifest.json"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
